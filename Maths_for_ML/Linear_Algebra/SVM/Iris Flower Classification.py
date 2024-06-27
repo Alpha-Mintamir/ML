ba9d94ee2689b8f8e378f7f73c5508fa43afe7ee
Alpha Lencho <Alphalenchoo@gmail.com>
2024-06-27 12:11:50 +0300

    Train-Test Split

--- Maths_for_ML/Linear_Algebra/SVM/Iris Flower Classification.py
+++ Maths_for_ML/Linear_Algebra/SVM/Iris Flower Classification.py
@@ -1,5 +1,7 @@
 from sklearn import datasets
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 
 iris = datasets.load_iris()
@@ -10,3 +12,5 @@ print(iris_df.head())
 
 X = iris_df.drop('species', axis=1)  
 y = iris_df['species']  
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
