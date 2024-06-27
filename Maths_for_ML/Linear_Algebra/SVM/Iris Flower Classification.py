05e3a4b519145d6e3a94e078e183bdbed7c33e61
Alpha Lencho <Alphalenchoo@gmail.com>
2024-06-27 12:14:07 +0300

    Feature Scaling

--- Maths_for_ML/Linear_Algebra/SVM/Iris Flower Classification.py
+++ Maths_for_ML/Linear_Algebra/SVM/Iris Flower Classification.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 
 
 iris = datasets.load_iris()
@@ -14,3 +15,11 @@ X = iris_df.drop('species', axis=1)
 y = iris_df['species']  
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
