5316f84227a5e3fe536e6aba221bd92a67074ca5
Alpha Lencho <Alphalenchoo@gmail.com>
2024-06-29 23:14:14 +0300

    Encoded the labels (ham=0, spam=1)

--- Maths_for_ML/Linear_Algebra/SVM/Spam sms filtering/spam sms filter.py
+++ Maths_for_ML/Linear_Algebra/SVM/Spam sms filtering/spam sms filter.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip"
 df = pd.read_csv(url, sep='/t', names=['label', 'message'], compression='zip')
+
+#encoding the labels
+label_encoder = LabelEncoder()
+df['label'] = label_encoder.fit_transform(df['label'])
