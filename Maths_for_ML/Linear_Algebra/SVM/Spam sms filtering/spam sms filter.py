21c45114e3326eb08cfaf2afffee6d203aa67eef
Alpha Lencho <Alphalenchoo@gmail.com>
2024-06-29 23:25:48 +0300

    after splitting the data to train, and test, converted the text to numerical data

--- Maths_for_ML/Linear_Algebra/SVM/Spam sms filtering/spam sms filter.py
+++ Maths_for_ML/Linear_Algebra/SVM/Spam sms filtering/spam sms filter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip"
@@ -8,3 +10,15 @@ df = pd.read_csv(url, sep='/t', names=['label', 'message'], compression='zip')
 #encoding the labels
 label_encoder = LabelEncoder()
 df['label'] = label_encoder.fit_transform(df['label'])
+
+
+# Split the data into training and testing sets
+
+x_train, x_test, y_train, y_test = train_test_split(df['message'], df['label'], test_size=0.3, random_state=42)
+
+#convert the text to numerical value
+
+vector = CountVectorizer()
+
+x_train = vector.fit_transform(x_train)
+x_test = vector.fit(x_test)
